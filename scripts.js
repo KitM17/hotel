d14f5c4915a7013cf9774fb466fc480d1e6994df
KitM17 <maria-v-52rus@yandex.ru>
2022-08-09 10:45:21 +0300

    слайдер

--- scripts.js
+++ scripts.js
@@ -63,3 +63,46 @@ function init() {
             e.get('target').options.unset('preset');
         });
 }
+$(function () {
+    $('#calendar2').daterangepicker({
+        "locale": {
+            "format": "DD.MM.YYYY",
+            "separator": " - ",
+            "applyLabel": "Применить",
+            "cancelLabel": "Отменить",
+            "fromLabel": "From",
+            "toLabel": "To",
+            "customRangeLabel": "Custom",
+            "weekLabel": "W",
+            "daysOfWeek": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            "monthNames": [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            "firstDay": 1
+        },
+        "startDate": new Date(),
+
+    }, function (start, end, label) {
+
+    });
+});
+
