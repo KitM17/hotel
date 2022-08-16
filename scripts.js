692808a5129062c82bb6c6d2cbc8c5d2bdce1e98
KitM17 <maria-v-52rus@yandex.ru>
2022-08-16 16:30:03 +0300

    media

--- scripts.js
+++ scripts.js
@@ -1,108 +1,70 @@
 $(function () {
-    $('#calendar').daterangepicker({
-        "locale": {
-            "format": "DD.MM.YYYY",
-            "separator": " - ",
-            "applyLabel": "Применить",
-            "cancelLabel": "Отменить",
-            "fromLabel": "From",
-            "toLabel": "To",
-            "customRangeLabel": "Custom",
-            "weekLabel": "W",
-            "daysOfWeek": [
-                "Вс",
-                "Пн",
-                "Вт",
-                "Ср",
-                "Чт",
-                "Пт",
-                "Сб"
-            ],
-            "monthNames": [
-                "Январь",
-                "Февраль",
-                "Март",
-                "Апрель",
-                "Май",
-                "Июнь",
-                "Июль",
-                "Август",
-                "Сентябрь",
-                "Октябрь",
-                "Ноябрь",
-                "Декабрь"
-            ],
-            "firstDay": 1
-        },
-        "startDate": new Date(),
 
-    }, function (start, end, label) {
 
-    });
+	$('.calendar').daterangepicker({
+		"locale": {
+			"format": "DD.MM.YYYY",
+			"separator": " - ",
+			"applyLabel": "Применить",
+			"cancelLabel": "Отменить",
+			"fromLabel": "From",
+			"toLabel": "To",
+			"customRangeLabel": "Custom",
+			"weekLabel": "W",
+			"daysOfWeek": [
+				"Вс",
+				"Пн",
+				"Вт",
+				"Ср",
+				"Чт",
+				"Пт",
+				"Сб"
+			],
+			"monthNames": [
+				"Январь",
+				"Февраль",
+				"Март",
+				"Апрель",
+				"Май",
+				"Июнь",
+				"Июль",
+				"Август",
+				"Сентябрь",
+				"Октябрь",
+				"Ноябрь",
+				"Декабрь"
+			],
+			"firstDay": 1
+		},
+		"startDate": new Date(),
+
+	}, function (start, end, label) {
+
+	});
 });
+
+
+
 ymaps.ready(init);
 
 function init() {
-    var myMap = new ymaps.Map('map', {
-        center: [56.645523, 43.463810],
-        zoom: 9
-    }, {
-        searchControlProvider: 'yandex#search'
-    }),
-        myPlacemark = new ymaps.Placemark(myMap.getCenter());
+	var myMap = new ymaps.Map('map', {
+		center: [56.645523, 43.463810],
+		zoom: 9
+	}, {
+		searchControlProvider: 'yandex#search'
+	}),
+		myPlacemark = new ymaps.Placemark(myMap.getCenter());
 
-    myMap.geoObjects.add(myPlacemark);
+	myMap.geoObjects.add(myPlacemark);
 
-    myPlacemark.events
-        .add('mouseenter', function (e) {
-            // Ссылку на объект, вызвавший событие,
-            // можно получить из поля 'target'.
-            e.get('target').options.set('preset', 'islands#greenIcon');
-        })
-        .add('mouseleave', function (e) {
-            e.get('target').options.unset('preset');
-        });
+	myPlacemark.events
+		.add('mouseenter', function (e) {
+			// Ссылку на объект, вызвавший событие,
+			// можно получить из поля 'target'.
+			e.get('target').options.set('preset', 'islands#greenIcon');
+		})
+		.add('mouseleave', function (e) {
+			e.get('target').options.unset('preset');
+		});
 }
-$(function () {
-    $('#calendar2').daterangepicker({
-        "locale": {
-            "format": "DD.MM.YYYY",
-            "separator": " - ",
-            "applyLabel": "Применить",
-            "cancelLabel": "Отменить",
-            "fromLabel": "From",
-            "toLabel": "To",
-            "customRangeLabel": "Custom",
-            "weekLabel": "W",
-            "daysOfWeek": [
-                "Вс",
-                "Пн",
-                "Вт",
-                "Ср",
-                "Чт",
-                "Пт",
-                "Сб"
-            ],
-            "monthNames": [
-                "Январь",
-                "Февраль",
-                "Март",
-                "Апрель",
-                "Май",
-                "Июнь",
-                "Июль",
-                "Август",
-                "Сентябрь",
-                "Октябрь",
-                "Ноябрь",
-                "Декабрь"
-            ],
-            "firstDay": 1
-        },
-        "startDate": new Date(),
-
-    }, function (start, end, label) {
-
-    });
-});
-
